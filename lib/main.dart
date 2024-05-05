68ad9e3b74f0c1975ee9688fee4af8d0a0ddc64b
Enes Ugur <eugurx@gmail.com>
2024-05-05 15:17:59 +0300

    Native splash removed

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:takenote/constants/style.dart';
 import 'package:takenote/views/home_page.dart';
 
 Future<void> main() async {
-  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-  FlutterNativeSplash.remove();
   runApp(const MyApp());
 }
 
